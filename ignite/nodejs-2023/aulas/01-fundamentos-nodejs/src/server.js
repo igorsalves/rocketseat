6a3fe05f72c25c131874fbab18bdf70e1d55f31e
Igor Silva Alves <igorsaj@hotmail.com>
2023-11-25 11:37:43 -0300

    feat: rotas de criação e listagem (Métodos HTTP)

--- ignite/nodejs-2023/aulas/01-fundamentos-nodejs/src/server.js
+++ ignite/nodejs-2023/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,17 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World');
+  const {method, url} = req;
+
+  if(method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários');
+  }
+
+  if(method === 'POST' && url === '/users') {
+    return res.end('Creação de usuários');
+  }
+
+  return res.end('Hello World');
 });
 
 server.listen(3333);
