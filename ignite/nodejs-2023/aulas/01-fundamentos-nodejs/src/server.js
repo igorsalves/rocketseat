320cb6ca9c2573f27adbe7ca650edbc552d628a7
Igor Silva Alves <igorsaj@hotmail.com>
2023-11-25 13:23:55 -0300

    feat: conhecendo HTTP status codes

--- ignite/nodejs-2023/aulas/01-fundamentos-nodejs/src/server.js
+++ ignite/nodejs-2023/aulas/01-fundamentos-nodejs/src/server.js
@@ -18,10 +18,10 @@ const server = http.createServer((req, res) => {
       email: 'johndoe@example.com'
     });
 
-    return res.end('Creação de usuários');
+    return res.writeHead(201).end();
   }
 
-  return res.end('Hello World');
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
